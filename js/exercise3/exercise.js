34625081fb656356ac98c49b2177dcd64c7c5bd1
Marco <rubertomarco48@gmail.com>
2023-06-01 18:32:27 +0200

    add:exercise 2

---
+++ js/exercise3/exercise.js
@@ -0,0 +1,23 @@
+/* const sum = function (a, b) {
+  return a + b;
+} */
+let a = 5;
+let b = 6;
+let sum = (a, b) => a + b ;
+console.log(sum);
+
+const subtract = function (a, b) {
+  return a - b;
+}
+
+const multiply = function (a, b) {
+  return a * b;
+}
+
+const divide = function (a, b) {
+  return a / b;
+}
+
+const log = function (value) {
+  console.log(value);
+}
