42074270e7837866e9ebb26902db80be86701c2b
Marco <rubertomarco48@gmail.com>
2023-06-25 19:01:45 +0200

    add:exercise promise

--- js_21-40/exercise37-Promise/README.md
+++ js_21-40/exercise37-Promise/README.md
@@ -2,3 +2,6 @@
 
 Write a `promise`. If the variable called `number` is greater than 10, then it returns a resolve, otherwise it returns a reject.
 Then manage the promise with `then` and `catch` in case of any reject.
+
+Scrivi una "promessa". Se la variabile chiamata "numero" è maggiore di 10, restituisce una risoluzione, altrimenti restituisce un rifiuto.
+Quindi gestisci la promessa con "then" e "catch" in caso di rifiuto.
--- js_21-40/exercise37-Promise/exercise.js
+++ js_21-40/exercise37-Promise/exercise.js
@@ -1,1 +1,17 @@
 const number = 15;
+const myPromise = new Promise((resolve, reject) => {
+  // Esegui un'operazione asincrona qui
+  if (number > 10) {
+    let result = "il numero va bene";
+    /* operazione completata con successo */
+    resolve(result); // restituisce il risultato
+  } else {
+    let error = "il  numero non va bene";
+    reject(error); // restituisce l'errore
+  }
+});
+myPromise
+    .then((val)=>console.log(val))
+    .catch((err)=>console.error(err))
+    
+
