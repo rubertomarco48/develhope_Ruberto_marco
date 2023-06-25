a16ab1a78025d7a434f7d8082c749e87aa812b14
Marco <rubertomarco48@gmail.com>
2023-06-25 20:56:24 +0200

    fix exercise34

--- js_21-40/exercise34-Callback/exercise.js
+++ js_21-40/exercise34-Callback/exercise.js
@@ -1,11 +1,19 @@
 
-function printAsyncName(callback,string) {
+/* function printAsyncName(callback,string) {
     let x = setInterval(callback,1000);
     let y = setInterval(()=>console.log(string),2000)
     setTimeout(()=>clearInterval(x),1000);
     setTimeout(()=>clearInterval(y),2000);
 
-}
+} */
+function printAsyncName(callback, string) {
+    setTimeout(() => {
+      callback();
+      setTimeout(() => {
+        console.log(string);
+      }, 2000);
+    }, 1000);
+  }
 
 function hello() {
     console.log("Hello");
