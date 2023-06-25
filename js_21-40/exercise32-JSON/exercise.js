44de518aa2a69974628b44408b591b064cd2c483
Marco <rubertomarco48@gmail.com>
2023-06-25 20:51:33 +0200

    fix:exercise 32

--- js_21-40/exercise32-JSON/exercise.js
+++ js_21-40/exercise32-JSON/exercise.js
@@ -4,9 +4,13 @@ const person = {
   lastName: 'Rossi',
   age: 25,
 };
-
-const json = JSON.parse(JSON.stringify(person));
+const json =JSON.parse(JSON.stringify(
+  Object.fromEntries(
+    Object.entries(person).filter(([key]) => key === "id" || key === "age")
+  )
+));
+/* const json = JSON.parse(JSON.stringify(person));
 delete json.firstName;
-delete json.lastName;
+delete json.lastName; */
 console.log(json); // Should return: { id: 1, age: 25 }
 
