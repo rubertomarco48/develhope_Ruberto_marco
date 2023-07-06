ee8b43e3e862231092ab13b56e3f23d4905715ae
Marco <rubertomarco48@gmail.com>
2023-07-06 20:37:33 +0200

    finished

--- js_40-/ex42_Network_Request/index.js
+++ js_40-/ex42_Network_Request/index.js
@@ -1,3 +1,28 @@
-import { extraction } from "./export";
-let url = "https://jsonplaceholder.typicode.com/posts";
-extraction(url);
+const form = document.querySelector("form");
+  const title = document.querySelector("#todo-title");
+  const check = document.querySelector("#todo-completed");
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const datiPost = {
+      title: title.value,
+      completed: check.checked
+    }
+
+    try {
+      const risposta = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(datiPost),
+      });
+      const response = await risposta.json();
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  /* non lo spiega il post nelle lezioni e da internet non lo sto capendo */
