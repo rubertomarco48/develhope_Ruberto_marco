84aa1a910c0d7bf76771721d78c580ac5da634a6
Marco <rubertomarco48@gmail.com>
2023-07-21 23:45:21 +0200

    add:exercise 9 react

---
+++ react/es9-Conditional Rendering -If less than `65`/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Welcome } from "./Welcome";
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Welcome /* name ="Alfredo" */ /* age = "30" *//>
+      </div>
+    );
+  }
+}
---
+++ react/es9-Conditional Rendering -If less than `65`/src/Welcome.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Age } from "./Age";
+export class Welcome extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Welcome,<strong>{this.props.name}</strong>
+        </p>
+        {this.props.age && this.props.age<= 65 && this.props.age>=18 && <Age age={this.props.age} />}
+      </div>
+    );
+  }
+}
+
+Welcome.defaultProps = { name: "Mario", age: 64};
