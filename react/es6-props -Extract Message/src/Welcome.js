3c65afe1dc32bb53b2a6a08145d171d6161d5ff7
Marco <rubertomarco48@gmail.com>
2023-07-21 17:10:02 +0200

    add :exercise 6

---
+++ react/es6-props -Extract Message/src/Welcome.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Age } from "./Age";
+export class Welcome extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Welcome,<strong>{this.props.name}</strong>
+        </p>
+        <Age age={this.props.age} />
+      </div>
+    );
+  }
+}
+Welcome.defaultProps = { name: "Mario", age: 18 };
