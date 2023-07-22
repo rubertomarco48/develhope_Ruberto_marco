7d2c42e26d9d4c3569bfc610385b989a1c27559c
Marco <rubertomarco48@gmail.com>
2023-07-22 16:52:17 +0200

    add:exercise 14 react

---
+++ react/es14-State-CounterDisplay component/my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Counter } from "./counter";
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Counter base= {12} interval={1000} increment={2} />
+      </div>
+    );
+  }
+}
---
+++ react/es14-State-CounterDisplay component/my-app/src/CounterDisplay.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export class CounterDisplay extends React.Component {
+ 
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: this.props.base,
+    };
+  }
+
+  componentDidMount() {
+    this.int = setInterval(() => {
+      this.setState((State) => ({
+        count: State.count + this.props.increment,
+      }));
+    }, this.props.interval);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Conteggio {this.state.count}</h1>
+      </div>
+    );
+  }
+}
