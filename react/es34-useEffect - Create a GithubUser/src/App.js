743e1beeff9aea0fc424c901eec849c38245c610
Marco <rubertomarco48@gmail.com>
2023-08-13 18:39:46 +0200

    add

---
+++ react/es34-useEffect - Create a GithubUser/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FetchComponent } from "./FetchComponent";
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+       <FetchComponent username="Camrotez"/>
+      </div>
+    );
+  }
+}
