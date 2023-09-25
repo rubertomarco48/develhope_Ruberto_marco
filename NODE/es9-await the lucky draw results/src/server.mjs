7fb47c86efc05090bfb0741205aef3f0ee19580f
Marco <rubertomarco48@gmail.com>
2023-09-25 09:57:16 +0200

    fix9

--- NODE/es9-await the lucky draw results/src/server.mjs
+++ NODE/es9-await the lucky draw results/src/server.mjs
@@ -1,43 +1,37 @@
-
 /* import fs from "node:fs"
 
   fs.writeFile("message.txt","Hello Develhope",(err)=>err && console.error(err))
 
   console.log("File Create/overWrite"); */
 
-
-
-
-
-
 /*   La funzione luckyDraw restituisce una promessa. 
   Crea una catena di promesse in cui la funzione è richiesta
    per ciascuno dei giocatori: Joe, Caroline e Sabrina */
 
-  function luckyDraw(player) {
-    return new Promise((resolve, reject) => {
-      const win = Boolean(Math.round(Math.random()));
-  
-      process.nextTick(() => {
-        if (win) {
-          resolve(`${player} won a prize in the draw!`);
-        } else {
-          reject(new Error(`${player} lost the draw.`));
-        }
-      });
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
+
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
     });
-  }
- 
-  const players = ["Joe", "Caroline", "Sabrina"];
-
-
-async function Gioco (){
-  players.forEach(async element => {
-    await luckyDraw(element)
-    .then((res)=>{
-      console.log(res);
-    })
-    .catch(err=>console.log(err.message))
   });
 }
-Gioco()
+
+const players = ["Joe", "Caroline", "Sabrina"];
+
+async function Gioco() {
+  players.forEach(async (element) => {
+    try {
+      const res = await luckyDraw(element);
+      console.log(res);
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+}
+Gioco();
