7d204a0f5ef568f5959f14b7978473737bf6c691
Marco <rubertomarco48@gmail.com>
2023-10-02 11:50:23 +0200

    fix10

--- typescript/es10-make an interface generic.ts
+++ typescript/es10-make an interface generic.ts
@@ -1,19 +1,17 @@
-/**
- * START: Follow the instructions below.
- */
-
 // Make this `LanguagesType` interface generic.
 // This should fix the type errors on Lines 14 and 21.
 // Hint: The type for the `languages` property should use a type variable.
 
-interface Country <Type>{
+interface Country<Type> {
     name: string;
     languages: Type;
 }
+
 interface LanguagesType<Type> {
-    name:Type;
-    languages:Type;
+    name: string;
+    languages: Type;
 }
+
 const languagesObj1: LanguagesType<string> = {
     name: "New Zealand",
     languages: "English, Māori"
@@ -27,7 +25,5 @@ const languagesObj2: Country<string[]> = {
 };
 
 console.log(languagesObj2.languages.join(", "));
-
-// ----
 
 export {};
