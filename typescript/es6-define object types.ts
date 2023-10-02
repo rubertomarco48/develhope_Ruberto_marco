40cb2f5f9a2a81ae02406a2b674fd185fcf85dd3
Marco <rubertomarco48@gmail.com>
2023-10-02 11:33:49 +0200

    fix6

--- typescript/es6-define object types.ts
+++ typescript/es6-define object types.ts
@@ -4,15 +4,15 @@
 
 // Add a type annotation to this variable that describes the object it contains.
 
-const countryPopulation:any = {
+const countryPopulation:{code: String;population: Number;} = {
     code: "NZ",
     population: 5_135_300,
 };
 
 // Define a `Country` interface that describes the object in this variable.
 // Add the `Country` type as a type annotation for this variable.
-type Country = any;
-const countryData:Country = {
+
+const countryData:{name:String;code:String;population:Number;} = {
     name: "India",
     code: "IN",
     population: 1_352_642_280,
@@ -20,8 +20,8 @@ const countryData:Country = {
 
 // Define a `Currency` type alias that describes the object in this variable.
 // Add the `Currency` type as a type annotation for this variable.
-type Currency = any;
-const currencyData:Currency = {
+
+const currencyData:{name:String;code:String;symbol:String} = {
     name: "Euro",
     code: "EUR",
     symbol: "€",
