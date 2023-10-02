57152fc3c10f987c0776bf1b946c8a6855f7b585
Marco <rubertomarco48@gmail.com>
2023-10-02 11:56:35 +0200

    fix15

--- typescript/es15-complete the class.ts
+++ typescript/es15-complete the class.ts
@@ -3,65 +3,38 @@
  */
 
 class Currency {
-    // Add types for these fields and make them private.
-    // Hint: Look at the type of values being passed when `new Currency()`
-    // is called below.
+    private name: string;
+    private code: string;
+    private symbol: string;
 
-   readonly name:string="";
-   readonly code:string="";
-   readonly symbol:string="";
-
-    // Add types to the constructor parameters.
-
-    constructor(name:string, code:string, symbol:string) {
+    constructor(name: string, code: string, symbol: string) {
         this.name = name;
-        this.code=code;
-        this.symbol=symbol;
-
-        // Store the `code` and `symbol` parameter values in
-        // their corresponding class fields.
-
-
+        this.code = code;
+        this.symbol = symbol;
     }
 
-    // Methods are public by default.
-    // Add a visibility modifier to make it clear this method is public.
-    //
-    // Add a return type for this method
-    // Hint: Look at the inferred return type.
-
-    describe() {
-        // Replace the `null` values below with the appropriate field values.
-        // Hint: Access field values with: this.fieldName
-
+    
+    public describe(): string {
+        
         let description = `The ${this.name} currency `;
         description += `has the code ${this.code} `;
         description += `and uses the symbol ${this.symbol}.`;
 
-        console.log(description);
+        return description;
     }
 }
 
-// ----
-
 const currencyNaira = new Currency("Naira", "NGN", "₦");
-
 console.log(currencyNaira);
-currencyNaira.describe()
-// Call the `describe()` method on the `currencyNaira` object.
 
 
-
-// ----
+console.log(currencyNaira.describe());
 
 const currencyUsDollar = new Currency("United States dollar", "USD", "$");
-
 console.log(currencyUsDollar);
 
-// Call the `describe()` method on the `currencyUsDollar` object.
 
+console.log(currencyUsDollar.describe());
 
-currencyUsDollar.describe()
-// ----
 
 export {};
